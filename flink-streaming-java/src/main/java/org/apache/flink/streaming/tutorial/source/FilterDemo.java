32eaa533ef467032a9e180a509b1163c1ac93ccd
JiGuoDing <485204300@qq.com>
2025-09-22 14:00:42 +0800

    Drop unused import. <M1Pro>
    Signed-off-by: JiGuoDing <485204300@qq.com>

--- flink-streaming-java/src/main/java/org/apache/flink/streaming/tutorial/source/FilterDemo.java
+++ flink-streaming-java/src/main/java/org/apache/flink/streaming/tutorial/source/FilterDemo.java
@@ -1,7 +1,6 @@
 package org.apache.flink.streaming.tutorial.source;
 
 import org.apache.flink.api.common.functions.FilterFunction;
-import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
