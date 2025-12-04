f5cec45a3756e461cf6818632fe82fd6c19284ae
JiGuoDing <485204300@qq.com>
2025-12-04 20:01:48 +0800

    feat: Enhance comments for clarity in AbstractRocksDBState and RocksDBValueState
    Signed-off-by: JiGuoDing <485204300@qq.com>

--- flink-state-backends/flink-statebackend-rocksdb/src/main/java/org/apache/flink/contrib/streaming/state/RocksDBValueState.java
+++ flink-state-backends/flink-statebackend-rocksdb/src/main/java/org/apache/flink/contrib/streaming/state/RocksDBValueState.java
@@ -98,6 +98,9 @@ class RocksDBValueState<K, N, V> extends AbstractRocksDBState<K, N, V>
             return;
         }
 
+        /*
+        * 在默认情况下，不同的 keyed state (例如 join 的 left state / right state) 各自对应一个 RocksDB column family
+         */
         try {
             backend.db.put(
                     columnFamily,
